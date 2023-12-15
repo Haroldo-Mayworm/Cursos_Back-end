c3901d166712d6dbfa1ebf59d4b58c266d2b5f38
Haroldo H. M. Mayworm <haroldomayworm@gmail.com>
2023-12-15 12:17:13 -0300

    feat(class): Add Java static modifier, static methods, and static initialization block
    Class files covering the use of static modifier, static methods, and static initialization blocks in Java. Organizing code writing using static.

---
+++ java_DevDojo/08-Static/02-methodsStatic/domain/Car.java
@@ -0,0 +1,46 @@
+package devDojo.Static.methodsStatic.domain;
+
+public class Car {
+    private String name;
+    private double maxSpeed;
+    private static double limitSpeed = 250;
+
+    public Car(String name, double maxSpeed) {
+        this.name = name;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void print() {
+        System.out.println("____________________");
+        System.out.println("Name: " + this.name);
+        System.out.println("Maximum speed: " + this.maxSpeed);
+        System.out.println("Limit speed: " + Car.limitSpeed);
+    }
+
+    // Getter e Setter - Static
+    public static double getLimitSpeed() {
+        return Car.limitSpeed;
+    }
+
+    public static void setLimitSpeed(double limitSpeed) {
+        Car.limitSpeed = limitSpeed;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMaxSpeed(double maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+}
---
+++ java_DevDojo/08-Static/02-methodsStatic/test/CarTest.java
@@ -0,0 +1,17 @@
+package devDojo.Static.methodsStatic.test;
+
+import devDojo.Static.methodsStatic.domain.Car;
+
+public class CarTest {
+    public static void main(String[] args) {
+        Car c1 = new Car("Audi", 290);
+        Car c2 = new Car("Volvo", 255);
+        Car c3 = new Car("Audi", 290);
+
+        Car.setLimitSpeed(180);
+
+        c1.print();
+        c2.print();
+        c3.print();
+    }
+}

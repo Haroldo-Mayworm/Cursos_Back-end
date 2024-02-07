27f04a4423b4185b1e2c171bf958df6365d11687
Haroldo H. M. Mayworm <haroldomayworm@gmail.com>
2024-02-06 21:28:19 -0300

    Feat: Add a database connection, read data from the table, and print the data to the screen

--- NodeJS/app/routes/news.js
+++ NodeJS/app/routes/news.js
@@ -1,5 +1,16 @@
+var DBConnection = require('../../config/DBConnection');
+
 module.exports = function (app) {
+    var connection = DBConnection();
+
     app.get('/news', function (req, res) {
-        res.render('news/news');
+        connection.query('select * from news_list', function (error, result) {
+            if (error) {
+                console.error('Error executing query:', error);
+                res.status(500).send('Internal Server Error');
+                return;
+            }
+            res.render('news/news', { newsList: result });
+        });
     });
 };
